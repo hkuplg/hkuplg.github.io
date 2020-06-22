cec9d03a4aff8ffdbf67c9d4939f9255a73ebc9f
juda <judaplus@gmail.com>
2020-06-22 12:10:22 +0800

    add content

--- styles/index.js
+++ styles/index.js
@@ -1,17 +1,22 @@
-var hide_lists = function(cb) {
+var hide_lists = function() {
     $('#posts').fadeOut(300);
     $('#projects').fadeOut(300);
     $('#members').fadeOut(300);
+    $('#highlight').fadeOut(300);
     $('#posts-btn').removeClass('disabled');
     $('#projects-btn').removeClass('disabled')
-    $('#members-btn').removeClass('disabled')
+    $('#members-btn').removeClass('disabled');
+    $('#highlight-btn').removeClass('disabled');
 };
 var show_projects = function() {
     $('#posts-btn').removeClass('disabled');
     $('#members-btn').removeClass('disabled');
+    $('#highlight-btn').removeClass('disabled')
     $('#posts').fadeOut(300, function() {
         $('#members').fadeOut(300, function() {
-            $('#projects').fadeIn(300)
+            $('#highlight').fadeOut(300, function() {
+                $('#projects').fadeIn(300)
+            })
         })
     });
     $('#projects-btn').addClass('disabled');
@@ -19,9 +24,12 @@ var show_projects = function() {
 var show_posts = function() {
     $('#projects-btn').removeClass('disabled');
     $('#members-btn').removeClass('disabled');
+    $('#highlight-btn').removeClass('disabled')
     $('#projects').fadeOut(function() {
         $('#members').fadeOut(300, function() {
-            $('#posts').fadeIn(300)
+            $('#highlight').fadeOut(300, function() {
+                $('#posts').fadeIn(300)
+            })
         })
     });
     $('#posts-btn').addClass('disabled');
@@ -29,10 +37,26 @@ var show_posts = function() {
 var show_members = function() {
     $('#projects-btn').removeClass('disabled');
     $('#posts-btn').removeClass('disabled');
+    $('#highlight-btn').removeClass('disabled')
     $('#projects').fadeOut(function() {
         $('#posts').fadeOut(300, function() {
-            $('#members').fadeIn(300)
+            $('#highlight').fadeOut(300, function() {
+             $('#members').fadeIn(300)
+            })
         })
     });
     $('#members-btn').addClass('disabled');
+};
+var show_highlight = function() {
+    $('#projects-btn').removeClass('disabled');
+    $('#posts-btn').removeClass('disabled');
+    $('#members-btn').removeClass('disabled')
+    $('#projects').fadeOut(function() {
+        $('#posts').fadeOut(300, function() {
+            $('#members').fadeOut(300, function() {
+                $('#highlight').fadeIn(300)
+            })
+        })
+    });
+    $('#highlight-btn').addClass('disabled');
 };
